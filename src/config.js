5a38887d278709046c096a4400d319d57e7a053a
Robinson Calderon <robinson.calderon@avaldigitallabs.com>
2020-07-07 10:58:58 -0500

    ejemplo inicial modificado para proyecto

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   region: 'us-east-2',
   IdentityPoolId: '',
-  UserPoolId: 'us-east-2_JQijyu6kw',
-  ClientId: '42tro8ju7ct71de03r4e7gt1fk',
+  UserPoolId: '',
+  ClientId: '',
 }
